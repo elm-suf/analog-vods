1e58203f99df91a7de95ba676426afbb70f6f3fd
sofiane <elm.sufian@gmail.com>
2024-06-29 14:23:15 +0200

    feat: :sparkles: added spartan icons

---
+++ analog-vods/src/app/components/command.component.ts
@@ -0,0 +1,81 @@
+// import { Component } from '@angular/core';
+// import { provideIcons } from '@ng-icons/core';
+// import {
+//   lucideCalendar,
+//   lucideCog,
+//   lucidePlus,
+//   lucideSearch,
+//   lucideSmile,
+//   lucideUser,
+//   lucideWallet,
+// } from '@ng-icons/lucide';
+// import { HlmButtonDirective } from '@spartan-ng/ui-button-helm';
+// import { BrnCommandImports } from '@spartan-ng/ui-command-brain';
+// import { HlmCommandImports } from '@spartan-ng/ui-command-helm';
+// import { HlmIconComponent } from '@spartan-ng/ui-icon-helm';
+
+// @Component({
+//   selector: 'spartan-command-preview',
+//   standalone: true,
+//   imports: [
+//     BrnCommandImports,
+//     HlmCommandImports,
+//     HlmIconComponent,
+//     HlmButtonDirective,
+//   ],
+//   providers: [
+//     provideIcons({
+//       lucideSearch,
+//       lucideCalendar,
+//       lucideSmile,
+//       lucidePlus,
+//       lucideUser,
+//       lucideWallet,
+//       lucideCog,
+//     }),
+//   ],
+//   template: `
+//     <brn-cmd class="w-96" hlm>
+//       <hlm-cmd-input-wrapper>
+//         <hlm-icon name="lucideSearch" />
+//         <input placeholder="Type a command or search..." brnCmdInput hlm />
+//       </hlm-cmd-input-wrapper>
+//       <div *brnCmdEmpty hlmCmdEmpty>No results found.</div>
+//       <brn-cmd-list hlm>
+//         <brn-cmd-group hlm label="Suggestions">
+//           <button brnCmdItem hlm>
+//             <hlm-icon name="lucideCalendar" hlmCmdIcon />
+//             Calendar
+//           </button>
+//           <button brnCmdItem hlm>
+//             <hlm-icon name="lucideSmile" hlmCmdIcon />
+//             Search Emoji
+//           </button>
+//           <button brnCmdItem hlm>
+//             <hlm-icon name="lucidePlus" hlmCmdIcon />
+//             Calculator
+//           </button>
+//         </brn-cmd-group>
+//         <brn-cmd-separator hlm></brn-cmd-separator>
+//         <brn-cmd-group hlm label="Settings">
+//           <button brnCmdItem hlm>
+//             <hlm-icon name="lucideUser" hlmCmdIcon />
+//             Profile
+//             <hlm-cmd-shortcut>⌘P</hlm-cmd-shortcut>
+//           </button>
+//           <button brnCmdItem hlm>
+//             <hlm-icon name="lucideWallet" hlmCmdIcon />
+//             Billing
+//             <hlm-cmd-shortcut>⌘B</hlm-cmd-shortcut>
+//           </button>
+//           <button brnCmdItem hlm>
+//             <hlm-icon name="lucideCog" hlmCmdIcon />
+//             Settings
+//             <hlm-cmd-shortcut>⌘S</hlm-cmd-shortcut>
+//           </button>
+//         </brn-cmd-group>
+//       </brn-cmd-list>
+//     </brn-cmd>
+//   `,
+// })
+// export class CommandPreviewComponent {}
--- analog-vods/src/app/pages/(home).page.ts
+++ analog-vods/src/app/pages/(home).page.ts
@@ -1,10 +1,17 @@
 import { Component } from '@angular/core';
-import { hlmH1 } from '@spartan-ng/typography-helm';
+import { hlmH1 } from '@spartan-ng/ui-typography-helm';
+import { IconComponent } from '../components/icon.component';
 
 @Component({
   selector: 'analog-vods-home',
   standalone: true,
-  template: ` <h2 class="${hlmH1} ">ANALOG-VODS</h2> `,
+  imports: [IconComponent],
+  template: `
+    <div class="flex gap-1 items-end">
+      <app-icon />
+      <h2 class="${hlmH1}">ANALOG-VODS</h2>
+    </div>
+  `,
   host: {
     class: 'w-full grid place-items-center content-center border px-8',
   },
